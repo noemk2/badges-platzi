4b15fcc49b2df8a0dcb3bb92210470762381625f
Noe Huaccharaque <noemk2@gmail.com>
2019-04-19 13:28:17 -0500

    Updates

---
+++ precache-manifest.1692a071e618f58f4a5b8066ee5219ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc7ba26573f2e19896c",
+    "url": "/noemk2/badges-platzi/static/css/main.b537e14c.chunk.css"
+  },
+  {
+    "revision": "cfc7ba26573f2e19896c",
+    "url": "/noemk2/badges-platzi/static/js/main.cfc7ba26.chunk.js"
+  },
+  {
+    "revision": "e16c4851d562f5e6b13a",
+    "url": "/noemk2/badges-platzi/static/js/runtime~main.e16c4851.js"
+  },
+  {
+    "revision": "0bd15404632387ccfe5f",
+    "url": "/noemk2/badges-platzi/static/css/2.3249c8cb.chunk.css"
+  },
+  {
+    "revision": "0bd15404632387ccfe5f",
+    "url": "/noemk2/badges-platzi/static/js/2.0bd15404.chunk.js"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/noemk2/badges-platzi/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/noemk2/badges-platzi/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/noemk2/badges-platzi/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "2d926e478e78f991fbba1f1d330351a3",
+    "url": "/noemk2/badges-platzi/index.html"
+  }
+];
